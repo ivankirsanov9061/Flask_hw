b06b874a06a01a18dbf5b3d2f74b57bbb16a391f
Ivan Kirsanov <114870773+ivankirsanov9061@users.noreply.github.com>
2023-08-13 13:57:00 -0400

    Help
    Signed-off-by: Ivan Kirsanov <114870773+ivankirsanov9061@users.noreply.github.com>

--- routs.py
+++ routs.py
@@ -3,6 +3,6 @@ from views import AdView
 
 app = Flask("app")
 
-app.add_url_rule("/advertisements/<int:id_ad>/", view_func=AdView.as_view('advertisements_delete'),
+app.add_url_rule("/advertisements/<int:id_ad>/", view_func=AdView.as_view('advertisements_delete'), # Что означает int:id_ad?
                  methods=['DELETE', 'GET'])
 app.add_url_rule("/advertisements", view_func=AdView.as_view('advertisements_create'), methods=['POST'])
